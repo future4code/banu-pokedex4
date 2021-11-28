2772bbd5a2d55fc63531fa4ed681caea316e9f4b
Felipe Bernardes <84930952+Feliperazz@users.noreply.github.com>
2021-11-28 17:20:44 -0300

    pokedex

--- src/components/CardsHome/CardHome.jsx
+++ src/components/CardsHome/CardHome.jsx
@@ -4,19 +4,37 @@ import axios from "axios"
 
 
 export default function CardHome(props) {
-
+  const [pokemons, setPokemons] = useState([])
+  const [pokeName, setPokeName] = useState("")
 const [pokemon, setPokemon] = useState([])
 const [pokeShiny, setPokeShiny] = useState(false)
 
 const onClickShiny = () => {
   setPokeShiny(!pokeShiny)
-}
+};
+
+const changePokeName = (e) => {
+  setPokeName(e.target.value)
+};
+
+useEffect (() => {
+  axios
+  .get("https://pokeapi.co/api/v2/pokemon?limit=20")
+  .then(response => {
+      setPokemons(response.data.results)
+      console.log(response)
+  })
+  .catch((err) =>{
+    alert("algo deu errado", err)
+  })
+
+}, []);
 
 
 
 useEffect(() => {
   axios
-  .get(`https://pokeapi.co/api/v2/pokemon/${props.pokeName}`)
+  .get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
   .then((response) => {
     console.log("poke:", response.data)
     setPokemon(response.data);
@@ -27,7 +45,7 @@ useEffect(() => {
   })
   
     
-}, [props.pokeName]);
+}, [pokeName]);
 
 
 
@@ -38,6 +56,13 @@ useEffect(() => {
 
 
   return <div>
+    <h1>HOME</h1>
+    <select onChange={changePokeName}>
+      <option value={""}> Nenhum</option>
+      {pokemons.map((pokemon) => (
+        <option value={pokemon.name}>{pokemon.name}</option>
+      ))}
+      </select>
    <p>{pokemon.name}</p>
      {pokemon.moves &&  pokemon.moves.map((item, i) => i <= 3 ?   <p>{item.move.name}</p> : null)}
       {pokemon.types && pokemon.types.map((item, i) => <p>{item.type.name}</p>)}
@@ -49,5 +74,6 @@ useEffect(() => {
       )}
       <button onClick={onClickShiny}>Shiny</button>
       <button onClick={props.trocaTela}>Ir para Pokedex</button>
+      <button>Add Pokedex</button>
   </div>;
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,28 +6,14 @@ import {useState, useEffect} from "react";
 
 
 export default function Home() {
-  const [pokemons, setPokemons] = useState([])
-  const [pokeName, setPokeName] = useState("")
+
   const [gerenciarTela, setGerenciarTela] = useState(true);
 
   
 
-  useEffect (() => {
-      axios
-      .get("https://pokeapi.co/api/v2/pokemon?limit=151")
-      .then(response => {
-          setPokemons(response.data.results)
-          console.log(response)
-      })
-      .catch((err) =>{
-        alert("algo deu errado", err)
-      })
+  
 
-  }, []);
-
-  const changePokeName = (e) => {
-    setPokeName(e.target.value)
-  };
+ 
 
   const trocaTela = () => {
     setGerenciarTela(!gerenciarTela);
@@ -40,16 +26,13 @@ export default function Home() {
 
   return (
     <div>
-      <h1>HOME</h1>
-      <select onChange={changePokeName}>
-      <option value={""}> Nenhum</option>
-      {pokemons.map((pokemon) => (
-        <option value={pokemon.name}>{pokemon.name}</option>
-      ))}
-      </select>
-      {pokeName && <CardHome pokeName={pokeName}/>}
+      
+      
+      
       {gerenciarTela ? (
-        <CardHome trocaTela={trocaTela} />
+      <CardHome  trocaTela={trocaTela}/>
+      
+
       ) : (
         <Pokedex trocaTela={trocaTela} />
       )}
